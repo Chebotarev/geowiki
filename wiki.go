55c81e27dc09e73ce5a0678726e44138fed90c5e
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2016-04-21 09:11:17 -0700

    Create hello world app

--- wiki.go
+++ wiki.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "io/ioutil"
+  "net/http"
 )
 
 type Page struct {
@@ -24,9 +25,11 @@ func load(title string) (*Page, error) {
   return &Page{Title: title, Body: body}, nil
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello World! You are at: %s!", r.URL.Path[1:])
+}
+
 func main() {
-  p1 := &Page{Title: "TestPage", Body: []byte("This is a sample page")}
-  p1.save()
-  p2, _ := load("TestPage")
-  fmt.Println(string(p2.Body))
+  http.HandleFunc("/", handler)
+  http.ListenAndServe(":8080", nil)
 }
